3f013ea306a3075c98d63989b22ae5dce253ffeb
Federico Calo <fedcal01@gmail.com>
2021-12-05 21:42:46 +0100

    sezione ordini

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ const expressLayout= require('express-ejs-layouts')
 const PORT=process.env.PORT || 3000
 
 app.use(express.static('public'))
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+
+
+
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.get('/carrello', (req,res) => {  
+    res.render('customers/carrello')
+})
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
